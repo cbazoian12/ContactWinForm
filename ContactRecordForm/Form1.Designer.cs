0523a4694e87c927aa6def538ee067feddf88260
Christian Bazoian <cbazoian12@wou.edu>
2016-03-05 00:29:00 -0800

    First Commit

---
+++ ContactRecordForm/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿using System.Data;
+
+namespace ContactRecordForm
+{
+    partial class RecordDateGridView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.RecordDataGridView = new System.Windows.Forms.DataGridView();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.FormatLabelInstructions = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.RecordDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(494, 376);
+            this.CloseButton.Margin = new System.Windows.Forms.Padding(2);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(93, 37);
+            this.CloseButton.TabIndex = 0;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // RecordDataGridView
+            // 
+            this.RecordDataGridView.AllowUserToOrderColumns = true;
+            this.RecordDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.RecordDataGridView.Location = new System.Drawing.Point(10, 11);
+            this.RecordDataGridView.Margin = new System.Windows.Forms.Padding(2);
+            this.RecordDataGridView.MultiSelect = false;
+            this.RecordDataGridView.Name = "RecordDataGridView";
+            this.RecordDataGridView.ReadOnly = true;
+            this.RecordDataGridView.RowTemplate.Height = 24;
+            this.RecordDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.RecordDataGridView.Size = new System.Drawing.Size(579, 251);
+            this.RecordDataGridView.TabIndex = 1;
+            this.RecordDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.RecordDataGridView_CellContentClick);
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(10, 316);
+            this.SearchButton.Margin = new System.Windows.Forms.Padding(2);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(94, 34);
+            this.SearchButton.TabIndex = 2;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(108, 317);
+            this.AddButton.Margin = new System.Windows.Forms.Padding(2);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(94, 33);
+            this.AddButton.TabIndex = 3;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(206, 317);
+            this.DeleteButton.Margin = new System.Windows.Forms.Padding(2);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(94, 33);
+            this.DeleteButton.TabIndex = 4;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.Location = new System.Drawing.Point(388, 376);
+            this.RefreshButton.Margin = new System.Windows.Forms.Padding(2);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(101, 37);
+            this.RefreshButton.TabIndex = 5;
+            this.RefreshButton.Text = "Refresh";
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(10, 292);
+            this.InputTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(580, 20);
+            this.InputTextBox.TabIndex = 6;
+            this.InputTextBox.TextChanged += new System.EventHandler(this.InputTextBox_TextChanged);
+            // 
+            // FormatLabelInstructions
+            // 
+            this.FormatLabelInstructions.AutoSize = true;
+            this.FormatLabelInstructions.Location = new System.Drawing.Point(13, 274);
+            this.FormatLabelInstructions.Name = "FormatLabelInstructions";
+            this.FormatLabelInstructions.Size = new System.Drawing.Size(244, 13);
+            this.FormatLabelInstructions.TabIndex = 7;
+            this.FormatLabelInstructions.Text = "Format: Name,House Number,Street,City,State,Zip";
+            this.FormatLabelInstructions.Click += new System.EventHandler(this.FormatLabelInstructions_Click);
+            // 
+            // RecordDateGridView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 424);
+            this.Controls.Add(this.FormatLabelInstructions);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.RefreshButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.RecordDataGridView);
+            this.Controls.Add(this.CloseButton);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "RecordDateGridView";
+            this.Text = "Contacts";
+            ((System.ComponentModel.ISupportInitialize)(this.RecordDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.DataGridView RecordDataGridView;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button RefreshButton;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Label FormatLabelInstructions;
+    }
+}
+
---
+++ ContactRecordForm/Form1.cs
@@ -0,0 +1,303 @@
+﻿///<author>Christian Bazoian</author>
+/// <date>March 4, 2016</date>
+
+using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ContactRecordForm
+{
+    /// <summary>
+    /// This is a program that uses a WinForm to communicate with a flat file. The flat file contains a collection of names
+    /// and mailing addresses. The form can be used to search, add, and delete records using the flat file.
+    /// </summary>
+    public partial class RecordDateGridView : Form
+    {
+        public RecordDateGridView()
+        {
+            InitializeComponent();
+            DataGridViewSetup();
+        }
+
+        /// <summary>
+        /// Sets up the Data Grid View to display the current list of contacts.
+        /// </summary>
+        private void DataGridViewSetup()
+        {
+            FileSetUp("contacts.txt");
+        }
+
+        /// <summary>
+        /// The listener that responds to any cells that are clicked.
+        /// </summary>
+        /// <param name="sender">Provides access to the sender object.</param>
+        /// <param name="e">Provides access to the DataGridView event.</param>
+        private void RecordDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (RecordDataGridView.CurrentCell != null && RecordDataGridView.CurrentCell.Value != null) //if cell and contentents are not null
+            {
+                InputTextBox.Text = GetDataGridRow();
+            }
+            else
+            {
+                InputTextBox.Text = "No Selection";
+            }
+        }
+
+        /// <summary>
+        /// Listener for any changes to the input text box.
+        /// </summary>
+        /// <param name="sender">Provides access to the sender object.</param>
+        /// <param name="e">Provides access to the DataGridView event.</param>
+        private void InputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            //Do nothing
+        }
+
+        /// <summary>
+        /// Listener for the search button. When clicked, the contact flat file will be searched for whatever text is in the input text box.
+        /// </summary>
+        /// <param name="sender">Provides access to the sender object.</param>
+        /// <param name="e">Provides access to the DataGridView event.</param>
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            string input = InputTextBox.Text; //get input
+
+            string record = FormatRecord(input); //format input
+
+            File.Delete("search.txt"); //delete search file if it already exists
+
+            string line = null;
+
+            System.IO.StreamReader file = new System.IO.StreamReader("contacts.txt");
+            
+            //add column headers to new file that will contains searches
+            StreamWriter w = File.AppendText("search.txt");
+            w.WriteLine("Name HouseNumber Street City State Zip");
+            w.Close();
+
+            while((line = file.ReadLine()) != null)
+            {
+                if(line.Contains(record))
+                {
+                    using (StreamWriter sw = File.AppendText("search.txt"))
+                    {
+                        sw.WriteLine(line); //write the line of text from text box
+                    }
+                }
+            }
+
+            FileSetUp("search.txt"); //use the new file to set up the Data Grid View
+        }
+
+        /// <summary>
+        /// Listener for the add button. Record in the text box will be added to the flat file.
+        /// </summary>
+        /// <param name="sender">Provides access to the sender object.</param>
+        /// <param name="e">Provides access to the DataGridView event.</param>
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            string input = InputTextBox.Text; //get input
+
+            string record = FormatRecord(input); //format input
+
+            if (!ContactFileContains(input)) //if the contact doesn't already exist
+            {
+                var count = input.Count(x => x == ','); //find the number of commas in input
+
+                if(count == 5) //if there are at least five commas, it can be a valid record.
+                {
+                    using (StreamWriter w = File.AppendText("contacts.txt")) //appbend text to contact list.
+                    {
+                        w.WriteLine(record); //write the line of text from text box
+                        w.Flush();
+                    }
+                }
+                else
+                {
+                    InputTextBox.Text = "Invalid Format";
+                }
+            }
+            else
+            {
+                InputTextBox.Text = "Record already exists";
+            }
+
+            InputTextBox.Text = "";
+            RefreshButton_Click(null, null); //refresh the data grid
+        }
+
+        /// <summary>
+        /// Delete button listener. Deletes the file specified in the text box if it indeed exists.
+        /// </summary>
+        /// <param name="sender">Provides access to the sender object.</param>
+        /// <param name="e">Provides access to the DataGridView event.</param>
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            if (InputTextBox.Text != "" && InputTextBox.Text != null) //make sure text box is not empty
+            {
+                string input = InputTextBox.Text; //get input
+                string record = FormatRecord(input); //format input
+
+                string line = null;
+
+                using (StreamReader reader = new StreamReader("contacts.txt")) //read in contacts file
+                {
+                    using (StreamWriter writer = new StreamWriter("tempFile.txt")) //create a temp file to write to
+                    {
+                        while ((line = reader.ReadLine()) != null) //read in all lines in contacts file
+                        {
+                            if (line.Equals(record)) //if reading in a line that equals the line to delete
+                            {
+                                continue; //skip over it
+                            }
+                            writer.WriteLine(line); //write all lines to temp file 
+                        }
+                    }
+                }
+
+                File.Delete("contacts.txt"); //delete contacts file so it can be re-made
+
+                using (StreamReader reader = new StreamReader("tempFile.txt")) //open the temp file
+                {
+                    using (StreamWriter writer = new StreamWriter("contacts.txt")) //create a new contacts file to write to
+                    {
+                        while ((line = reader.ReadLine()) != null) //while there are still lines left
+                        {
+                            writer.WriteLine(line); //write line
+                        }
+                    }
+                }
+                File.Delete("tempFile.txt"); // delete the temp file
+                RefreshButton_Click(null, null); //refresh the data grid 
+                InputTextBox.Text = ""; //clear textbox
+            }
+            else
+            {
+                InputTextBox.Text = "No Selection";
+            }
+        }
+
+        /// <summary>
+        /// Listener for the Refresh button. Refreshes the data grid view to display most current flat file contents.
+        /// </summary>
+        /// <param name="sender">Provides access to the sender object.</param>
+        /// <param name="e">Provides access to the DataGridView event.</param>
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            RecordDataGridView.DataSource = null; //nullify the dataSource
+            RecordDataGridView.Rows.Clear(); //clear the rows
+            DataGridViewSetup();  //publish update contents to dataGridView
+        }
+
+        /// <summary>
+        /// Listener for the close button. Closes the WinForm.
+        /// </summary>
+        /// <param name="sender">Provides access to the sender object.</param>
+        /// <param name="e">Provides access to the DataGridView event.</param>
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Gets the contents of the currently selected row on the data grid view.
+        /// </summary>
+        /// <returns>A comma delimeted string representation of the currently selected row.</returns>
+        private string GetDataGridRow()
+        {
+            //get the contents of each cell of currently selected row and return it compiled together as a string.
+            string name = RecordDataGridView.CurrentRow.Cells[0].Value.ToString();
+            string houseNumber = RecordDataGridView.CurrentRow.Cells[1].Value.ToString();
+            string street = RecordDataGridView.CurrentRow.Cells[2].Value.ToString();
+            string city = RecordDataGridView.CurrentRow.Cells[3].Value.ToString();
+            string state = RecordDataGridView.CurrentRow.Cells[4].Value.ToString();
+            string zip = RecordDataGridView.CurrentRow.Cells[5].Value.ToString();
+            string text = name + "," + houseNumber + "," + street + "," + city + "," + state + "," + zip;
+            return text;
+        }
+
+        /// <summary>
+        /// Searches the contact flat file for any possible instance of the text in the intput text box.
+        /// </summary>
+        /// <param name="contact">The text to search for.</param>
+        /// <returns>A boolean value.</returns>
+        private bool ContactFileContains(string contact)
+        {
+            using (StreamReader reader = new StreamReader("contacts.txt")) //read in contacts file
+            {
+                string line = null;
+                while ((line = reader.ReadLine()) != null) //read in all lines in contacts file
+                {
+                    if (line.Equals(contact)) //if reading in a line that equals the line to delete
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// A listener for the instruction label. Does nothing.
+        /// </summary>
+        /// <param name="sender">Provides access to the sender object.</param>
+        /// <param name="e">Provides access to the DataGridView event.</param>
+        private void FormatLabelInstructions_Click(object sender, EventArgs e)
+        {
+            //Do nothing.
+        }
+
+        /// <summary>
+        /// Sets up the file with the data grid view.
+        /// </summary>
+        /// <param name="file">Path to the file.</param>
+        private void FileSetUp(string file)
+        {
+            RecordDataGridView.DataSource = null;
+            //When initialized, automatically bring up the current contact list
+            System.IO.StreamReader dataFile = new System.IO.StreamReader(file); //open flat file
+            string[] columnnames = dataFile.ReadLine().Split(' '); //split each line by spaces
+            DataTable dt = new DataTable(); //create a new data table to hold the information in.
+
+            foreach (string column in columnnames) //get each column name
+            {
+                dt.Columns.Add(column); //add column name to data file
+            }
+
+            string newLine; //Create a new string to hold each row of information in txt file
+
+            while ((newLine = dataFile.ReadLine()) != null) //while the file still has text in it
+            {
+                DataRow dr = dt.NewRow(); //create a new row in the data table
+                string[] values = newLine.Split(','); //split each entry in the flat file by spaces
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    dr[i] = values[i]; //enter each value from flat file into a data row
+                }
+
+                dt.Rows.Add(dr); //add the new data row into the data table
+            }
+
+            dataFile.Close();
+            RecordDataGridView.DataSource = dt; //set the data source for the data grid view as the data table just created.
+        }
+
+        /// <summary>
+        /// Formats a string to a format that the flat file is written in.
+        /// </summary>
+        /// <param name="record">The string to format.</param>
+        /// <returns></returns>
+        string FormatRecord(string record)
+        {
+            record.Replace(", ", ","); //remove any spaces after commas
+            record.Replace(" ,", ","); //remove any spaces before commas
+            record.Trim(); //remove any white space before or after the record
+            return record;
+        }
+        
+    }
+}
